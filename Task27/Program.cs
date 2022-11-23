3281eb4763fc6bb618c8adf69ac89f546476587e
Sofya <sofyaguseva1@gmail.comgit>
2022-11-23 15:20:37 +0100

    Task27 finished

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int rest = number;
+int sum = 0;
+
+while(rest != 0)
+{
+    sum += rest%10;
+    rest /= 10;
+}
+
+Console.WriteLine($"{number} -> {sum}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
