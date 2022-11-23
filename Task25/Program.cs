602880cbc51b61ce80805a701a5092b5f3574839
Sofya <sofyaguseva1@gmail.comgit>
2022-11-23 14:53:03 +0100

    Task25 finished

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// Без использования функции Math.Pow.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int number1 = ReadInt("Введите первое число: ");
+int number2 = ReadInt("Введите второе число: ");
+int result = 1;
+
+for (int i = 0; i < number2; i++)
+{
+    result *= number1;
+}
+
+Console.WriteLine($"Число {number1} в степени {number2} = {result}");
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
